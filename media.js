f0a3f909b544748bdeaad0dd94823b6d5928dcab
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-11 19:10:25 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,8 @@
+//Importando o módulo 
+const readline = require('readline-sync')
+
+const nota1 = parseInt(readline.question("Digite a primeira nota: \n"))
+const nota2 = parseInt(readline.question("Digite a segunda nota: \n"))
+const nota3 = parseInt(readline.question("Digite a terceira nota: \n"))
+
+console.log((nota1 + nota2 + nota3)/3)
